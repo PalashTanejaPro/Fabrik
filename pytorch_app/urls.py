7e64481b5262002cd654af02452cc7c19f5716a0
PalashTanejaPro <tanejapalash@gmail.com>
2018-01-17 02:21:35 +0530

    Fixed minor bug

--- pytorch_app/urls.py
+++ pytorch_app/urls.py
@@ -3,5 +3,5 @@ from views.import_pytorch import import_model
 
 
 urlpatterns = [
-    url(r'^import$', import_model, name='tf-import'),
+    url(r'^import$', import_model, name='pytorch-import'),
 ]
